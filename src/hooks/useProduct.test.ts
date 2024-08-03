fac01ef884237b8e2a05a0602844fb7be6002270
Cooper feetham <hello@cooperfeetham.com>
2024-08-03 19:25:43 +0100

    feat: added tests for useProduct hook

---
+++ src/hooks/useProduct.test.ts
@@ -0,0 +1,24 @@
+import { act } from "react";
+import { renderHook, waitFor } from "@testing-library/react";
+import { HARDCOVER_NOTEBOOK_PRODUCT_ID } from "utils/constants";
+import useProduct from "./useProduct";
+
+describe("useProduct hook", () => {
+  test("should initially output inital values for allOptions, selectedOptions, products, selectedProduct,", () => {
+    const { result } = renderHook(() => useProduct());
+    const { current } = result;
+    expect(current.allOptions).toBe(undefined);
+    expect(current.selectedOptions.length).toBe(0);
+    expect(current.products.length).toBe(0);
+    expect(current.selectedProduct).toBe(undefined);
+  });
+
+  test("should be populated with correct for allOptions, products, selectedProduct when getProducts is called getProducts", async () => {
+    const { result } = renderHook(() => useProduct());
+    await act(() => result.current.getProducts(HARDCOVER_NOTEBOOK_PRODUCT_ID));
+    expect(result.current.allOptions).toBeDefined();
+    expect(result.current.selectedOptions.length).toBe(0);
+    expect(result.current.products.length).toBeGreaterThan(0);
+    expect(result.current.selectedProduct).toBeDefined();
+  });
+});
